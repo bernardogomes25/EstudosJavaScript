63a7a78a3d157994afe73b3b613fcdb3f628e1c4
Gomes <be.gpereira25@gmail.com>
2025-05-14 17:38:04 -0300

    .

---
+++ Udemy/arq8/index.js
@@ -0,0 +1,11 @@
+let num = 10.562525235235235;
+console.log(num.toFixed(2));
+console.log(Number.isInteger(num));
+
+let num1 = 0.7;
+let num2 = 0.1;
+
+let soma = num1 + num2;
+soma = soma.toFixed(2);
+
+console.log(soma);

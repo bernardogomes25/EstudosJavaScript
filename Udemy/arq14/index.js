9314491f4e4a7f1dd7636ba90d6fab2a4d4d74d1
Gomes <be.gpereira25@gmail.com>
2025-05-19 11:58:57 -0300

    Programa básico sobre preenchimento de formulário

--- Udemy/arq14/index.js
+++ Udemy/arq14/index.js
@@ -1,7 +1,7 @@
 /*
-Primitivos (imutáveis) - String, number, boolean, undefined, null
+Primitivos (imutáveis) - String, number, boolean, undefined, null (bigint, symbol) - Valores copiados
 
-Referência (mutáveis) - Arrays, Objects, Functions
+Referência (mutáveis) - Arrays, Objects, Functions - Valores passados por referência
 */
 
 let a = [1,2,3,4,5];
